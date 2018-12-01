db196c5362a419a0646a318d17dd71644d047794
snolifer <svetlana.oliferchik@intel.com>
2018-12-01 15:44:46 -0800

    made changes

---
+++ src/Menu/MenuBarFrameDriver.java
@@ -0,0 +1,25 @@
+package Menu;
+
+/**
+ * An example that uses a menu bar
+ *
+ */
+public class MenuBarFrameDriver
+{
+
+    /**
+     * The entry point of the example
+     *
+     * @param args   The command line arguments
+     */
+    public static void main(String[] args)
+    {
+        MenuBar   f;
+
+        f = new MenuBar();
+        f.setSize(400,200);
+        f.setVisible(true);
+
+        
+    }
+}
