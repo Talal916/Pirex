13be0a15151228eac56dbcbf7fce8ed85af14076
sfang1236 <36020329+sfang1236@users.noreply.github.com>
2018-12-02 15:20:31 -0800

    completed SumListener

---
+++ src/indexer/SumListener.java
@@ -0,0 +1,43 @@
+package indexer;
+
+import java.util.HashMap;
+
+import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import indexer.Indexer;
+import tabGUI.SumTab;
+import textProcessor.ProcessedBook;
+
+public class SumListener implements ChangeListener {
+	private SumTab sumtab;
+	
+	public SumListener(SumTab st) {
+		this.sumtab = st;
+	}
+	
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		sumtab.getTextAreas().setText("");
+	    JTabbedPane sourceTabbedPane = (JTabbedPane) e.getSource();
+	    int index = sourceTabbedPane.getSelectedIndex();
+	    if (sourceTabbedPane.getTitleAt(index).equals("Summarize Documents")) {
+	    	HashMap<Integer, ProcessedBook> map = new HashMap<Integer, ProcessedBook>();
+	    	Indexer i = new Indexer();
+	    	if (map != null) {
+	    		for (Integer key : map.keySet()) {
+	    			ProcessedBook book = map.get(key);
+	    			StringBuilder sb = new StringBuilder();
+	    			sb.append("Opus   " + book.getOpusNum() + ": ");
+	    			sb.append(book.getAuthor() + "  " + book.getTitle());
+	    			sb.append("   " + book.getParagraphNum() + " documents\n");
+	    			sb.append("             " + book.getFile().getAbsolutePath() + "\n" );
+	    			sumtab.getTextAreas().append(sb.toString());
+	    		}
+	    		sumtab.getTextAreas().append("\nIndex terms: "+i.getIndexCount()+"\n");
+	    		sumtab.getTextAreas().append("Postings: "+i.getPostings());
+	    	}
+	    }
+	}
+}
